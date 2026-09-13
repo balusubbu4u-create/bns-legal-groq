1de13e84317c57e3dddb4797b1be75a3702ce1c6
balusubbu4u-create <balu.subbu4u@gmail.com>
2026-09-13 20:39:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ except Exception as e:
 # MODEL
 # =========================================================
 
-MODEL_NAME = "llama-3.3-70b-versatile"
+MODEL_NAME = "openai/gpt-oss-120b"
 
 
 # =========================================================
